0dcc34497822a8d600cefba393feea6a027bfad7
Lucas Nykvist <lucasnykvist01@gmail.com>
2024-01-02 08:15:48 +0100

    Add bcrypt and bcryptjs dependencies, and encrypt user password before insertion

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -16,20 +16,18 @@ import { User } from 'src/modules/users/entities/user.entity';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  //Create a user
   @Post()
   async createUser(@Body() user: User): Promise<User> {
     return await this.usersService.createUser(user);
   }
 
-  //Get all users
   @Get()
   async getAllUsers(@Res() res: Response): Promise<void> {
     const users = await this.usersService.getAllUsers();
 
     res.send(users);
   }
-  //Get Specific user
+
   @Get(':id')
   async getUserById(@Param('id') id: number): Promise<User> {
     return await this.usersService.getUserById(id);
