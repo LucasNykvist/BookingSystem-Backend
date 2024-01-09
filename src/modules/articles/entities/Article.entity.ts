0b9a327bd79a638460d71459cc8962587020e81f
Lucas Nykvist <lucasnykvist01@gmail.com>
2024-01-09 20:58:29 +0100

    Ent

--- src/modules/articles/entities/Article.entity.ts
+++ src/modules/articles/entities/Article.entity.ts
@@ -1,8 +1,29 @@
 import { UUID } from 'crypto';
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Article {
   @PrimaryGeneratedColumn('uuid')
   id: UUID;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  image: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
